670f582756fba8bc38fab424124be387ac8ea3b4
leshanydy2022 <98255172+leshanydy2022@users.noreply.github.com>
2022-05-10 21:36:28 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "36ff0020-9f07-43db-afea-1f35f8a3490a",
                         "alterId": 0
                     }
                 ],
